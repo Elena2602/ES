5048cd66bb9511540939ca55add7708c6ae9aff1
Elena2602 <118214409+Elena2602@users.noreply.github.com>
2022-12-06 21:36:53 +0300

    Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15
+Console.Clear();
+Console.Write("Введите день недели: ");
+  int n = Convert.ToInt32(Console.ReadLine());
+
+  while (n < 1 || n > 7)
+  {
+    Console.Write("Вы ошиблись!\nВведитете день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+  }
+if (n == 6 || n == 7)
+Console.WriteLine("Выходной");
+else  
+Console.WriteLine("Рабочий");
+
