0f152afcffc5bde49e7df8ee6958770d33e1d625
Elena2602 <118214409+Elena2602@users.noreply.github.com>
2022-12-06 21:18:24 +0300

    Tack_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 13
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n / 100 == 0)
+{
+    Console.WriteLine("Нет третьей цифры");
+    Console.WriteLine(n / 100);
+}
+if (n / 100 > 0)
+{
+    int n1 = 1000;
+    while (n / n1 > 0)
+    {
+        n1 = n1 * 10;
+    }
+    int n4 = n / (n1 / 1000);
+    int n3 = n4 % 10;
+    Console.WriteLine(n3);
+
+}
